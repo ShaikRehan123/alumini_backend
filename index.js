0f6ed8d3d5c2cf2e488e7bb8a9779e1c184abffd
SHAIK REHAN <YMTSIN50@ymts.com>
2022-06-10 16:32:16 +0530

    SO MUCH HARD WORK

--- index.js
+++ index.js
@@ -3,14 +3,37 @@ const bodyParser = require("body-parser");
 const app = express();
 const mysql = require("mysql");
 const PORT = process.env.PORT || 8080;
+const moment = require("moment");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 );
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -24,10 +47,11 @@ connection.connect(function (err) {
   }
   console.log(`Connected with threadID ${connection.threadId}`);
 });
+
 app.get("/", (req, res) => {
   res.send({
     status: 200,
-    message: "Hello Lol",
+    message: "Hello Noob",
   });
 });
 
@@ -49,6 +73,633 @@ app.get("/rooms", (req, res) => {
       });
 
       console.log(result);
+    }
+  );
+});
+
+app.post("/user_login", (req, res) => {
+  const { email, password } = req.body;
+  connection.query(
+    "SELECT * FROM `users` WHERE `u_email_id` = ? AND `u_password` = ?",
+    [email, password],
+    function (err, result) {
+      if (err)
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      else if (result.length > 0) {
+        res.send({
+          status: 200,
+          message: "Successfully Logged In",
+          data: JSON.stringify(result),
+        });
+      } else {
+        res.send({
+          status: 404,
+          message: "Invalid Credentials",
+        });
+      }
+
+      console.log(result);
+    }
+  );
+  // console.log(email, password);
+  // res.send({
+  //   status: 200,
+  //   message: "test",
+  //   data: "test",
+  // });
+});
+
+app.post("/admin_login", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+  connection.query(
+    "SELECT * FROM `admin` WHERE `username` = ? AND `password` = ?",
+    [email, password],
+    function (err, result) {
+      if (err)
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      else if (result.length > 0) {
+        res.send({
+          status: 200,
+          message: "Successfully Logged In",
+          data: JSON.stringify(result),
+        });
+      } else {
+        res.send({
+          status: 404,
+          message: "Invalid Credentials",
+        });
+      }
+
+      console.log(result);
+    }
+  );
+  // res.send({
+  //   status: 200,
+  //   message: "test",
+  //   data: "test",
+  // });
+});
+
+app.post("/update_user", (req, res) => {
+  const { name, username, mobile_no, email, password } = req.body;
+  // console.log(name, username, mobile_no, email, password);
+  connection.query(
+    "UPDATE `users` SET `u_name` = ?, `u_username` = ?, `u_mobileno` = ?, `u_email_id` = ?, `u_password` = ? WHERE `u_email_id` = ?",
+    [name, username, mobile_no, email, password, email],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        // get the updated user
+        connection.query(
+          "SELECT * FROM `users` WHERE `u_email_id` = ?",
+          [email],
+          function (err, result1) {
+            if (err)
+              res.send({
+                status: 500,
+                message: "Something is wrong on our side",
+                error: err,
+              });
+            else {
+              res.send({
+                status: 200,
+                message: "Successfully Updated",
+                data: JSON.stringify(result1),
+              });
+              console.log(result1);
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+app.get("/room_details/:id", (req, res) => {
+  const { id } = req.params;
+  connection.query(
+    "SELECT * FROM `room` WHERE `room_id` = ?",
+    [id],
+    function (err, result) {
+      if (err)
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      else {
+        res.send({
+          status: 200,
+          message: "Successfully Fetched",
+          data: result,
+        });
+        console.log(result);
+      }
+    }
+  );
+});
+
+app.post("/add_guest", (req, res) => {
+  const {
+    name,
+    email,
+    mobile,
+    userid,
+    booking_date,
+    arrival_from,
+    purpose_of_the_visit,
+    room_id,
+  } = req.body;
+  console.log(
+    name,
+    email,
+    userid,
+    booking_date,
+    arrival_from,
+    purpose_of_the_visit
+  );
+
+  // Check if already booked on the same date and same room_id
+  connection.query(
+    "select * from `transaction` where `checkin` = ? && `room_id` = ?",
+    [booking_date, room_id],
+    function (err, result) {
+      if (err)
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      else if (result.length > 0) {
+        res.send({
+          status: 404,
+          message: "Already Booked on this date",
+        });
+      } else {
+        connection.query(
+          "INSERT INTO `guest` (name,r_date,mobile,mail_id,createdDate,id_u) VALUES (?,?,?,?,?,?)",
+          [
+            name,
+            booking_date,
+            mobile,
+            email,
+            new Date().toISOString().slice(0, 19).replace("T", " "),
+            userid,
+          ],
+          function (err, result1) {
+            if (err) {
+              res.send({
+                status: 500,
+                message: "Something is wrong on our side",
+                error: err,
+              });
+            } else {
+              console.log(result1);
+              // Get the guest details
+              connection.query(
+                "SELECT * FROM `guest` WHERE `mail_id` = ?",
+                [email],
+                function (err, result2) {
+                  if (err)
+                    res.send({
+                      status: 500,
+                      message: "Something is wrong on our side",
+                      error: err,
+                    });
+                  else {
+                    // Insert into transaction table
+                    connection.query(
+                      "INSERT INTO `transaction`(guest_id, room_id, status, checkin,id_id,a_from,a_visit) VALUES (?,?,?,?,?,?,?)",
+                      [
+                        result2[0].guest_id,
+                        room_id,
+                        "Pending",
+                        booking_date,
+                        userid,
+                        arrival_from,
+                        purpose_of_the_visit,
+                      ],
+                      function (err, result2) {
+                        if (err) {
+                          res.send({
+                            status: 500,
+                            message: "Something is wrong on our side",
+                            error: err,
+                          });
+                        } else {
+                          res.send({
+                            status: 200,
+                            message: "Successfully Booked",
+                            data: result2,
+                          });
+                        }
+                      }
+                    );
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    }
+  );
+
+  // res.send({
+  //   status: 200,
+  //   message: "test",
+  //   data: "test",
+  // });
+});
+
+app.get("/user_bookings/:user_id", (req, res) => {
+  const { user_id } = req.params;
+  connection.query(
+    "SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE  id_id = ?",
+    [user_id],
+    function (err, result) {
+      if (err)
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      else {
+        res.send({
+          status: 200,
+          message: "Successfully Fetched",
+          data: result,
+        });
+        console.log(result);
+      }
+    }
+  );
+});
+
+app.get("/get_all_admins", (req, res) => {
+  connection.query("SELECT * FROM ADMIN", function (err, result) {
+    if (err) {
+      res.send({
+        status: 500,
+        message: "Something is wrong on our side",
+        error: err,
+      });
+    } else {
+      res.send({
+        status: 200,
+        message: "Successfully Fetched",
+        data: result,
+      });
+      console.log(result);
+    }
+  });
+});
+
+// Get adming by id
+app.get("/get_admin_by_id/", (req, res) => {
+  const { admin_id } = req.query;
+  console.log(admin_id);
+  connection.query(
+    "SELECT * FROM ADMIN WHERE admin_id = ?",
+    [admin_id],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        res.send({
+          status: 200,
+          message: "Successfully Fetched",
+          data: result,
+        });
+        console.log(result);
+      }
+    }
+  );
+});
+
+app.put("/update_admin", (req, res) => {
+  const { admin_id, name, username, password } = req.body;
+  console.log(admin_id, name, username, password);
+  connection.query(
+    "UPDATE ADMIN SET name = ?, username = ?, password = ? WHERE admin_id = ?",
+    [name, username, password, admin_id],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        res.send({
+          status: 200,
+          message: "Successfully Updated",
+          data: result,
+        });
+      }
+    }
+  );
+});
+
+app.get("/pending_reservations", (req, res) => {
+  connection.query(
+    "SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE `status` = 'Pending'",
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        // Get Number of pending reservations
+        connection.query(
+          "SELECT COUNT(*) AS count FROM `transaction` WHERE `status` = 'Pending'",
+          function (err, result1) {
+            if (err) {
+              res.send({
+                status: 500,
+                message: "Something is wrong on our side",
+                error: err,
+              });
+            } else {
+              res.send({
+                status: 200,
+                message: "Successfully Fetched",
+                data: result,
+                count: result1[0].count,
+              });
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+app.delete("/delete_reservation", (req, res) => {
+  const { transaction_id } = req.query;
+  connection.query(
+    "DELETE FROM `transaction` WHERE `transaction_id` = ?",
+    [transaction_id],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        res.send({
+          status: 200,
+          message: "Successfully Deleted",
+          data: result,
+        });
+      }
+    }
+  );
+});
+
+app.get("/get_transaction_by_id", (req, res) => {
+  const { transaction_id } = req.query;
+  console.log(transaction_id);
+  connection.query(
+    "SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE `transaction_id` = ?",
+    [transaction_id],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        res.send({
+          status: 200,
+          message: "Successfully Fetched",
+          data: result,
+        });
+        console.log(result);
+      }
+    }
+  );
+});
+
+app.post("/confirm_checkin", (req, res) => {
+  const [room_no, no_of_days, extra_bed, transaction_id] = [
+    req.body.room_no,
+    req.body.no_of_days,
+    req.body.extra_bed,
+    req.body.transaction_id,
+  ];
+  console.log(room_no, no_of_days, extra_bed, transaction_id);
+  connection.query(
+    "SELECT * FROM `transaction` WHERE `room_no` = ? && `status` = 'Check In'",
+    [room_no],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        if (result.length > 0) {
+          res.send({
+            status: 500,
+            message: "Room is already occupied",
+            data: result,
+          });
+        } else {
+          connection.query(
+            "SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE `transaction_id` = ?",
+            [transaction_id],
+            function (err, result1) {
+              if (err) {
+                res.send({
+                  status: 500,
+                  message: "Something is wrong on our side",
+                  error: err,
+                });
+              }
+              console.log(result1);
+              let total = result1[0].price * no_of_days;
+              let total2 = 800 * extra_bed;
+              const total_amount = total + total2;
+              // checkout date is the date of check in + no of days and format it ad y-m-d
+              // console.log(result1[0].check_in);
+              const checkout_date = moment(result1[0].checkin).add(
+                no_of_days,
+                "days"
+              );
+              const checkout_date_format =
+                moment(checkout_date).format("YYYY-MM-DD");
+              connection.query(
+                "UPDATE `transaction` SET `room_no` = ?, `days` = ?, `extra_bed` = ?, `status` = 'Check In', `checkin_time` = ?, `checkout` = ?, `bill` = ? WHERE `transaction_id` = ?",
+                [
+                  room_no,
+                  no_of_days,
+                  extra_bed,
+                  // it should be the current time like hours:minutes:seconds
+                  moment().format("HH:mm:ss"),
+                  checkout_date_format,
+                  total_amount,
+                  transaction_id,
+                ],
+                function (err, result2) {
+                  if (err) {
+                    res.send({
+                      status: 500,
+                      message: "Something is wrong on our side",
+                      error: err,
+                    });
+                  } else {
+                    res.send({
+                      status: 200,
+                      message: "Successfully Checked In",
+                      data: result2,
+                    });
+                  }
+                }
+              );
+            }
+          );
+        }
+      }
+    }
+  );
+});
+
+// get count of all three types of rooms
+app.get("/get_room_count", (req, res) => {
+  connection.query(
+    "SELECT COUNT(*) AS count FROM `transaction` WHERE `status` = 'Check In'",
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        connection.query(
+          "SELECT COUNT(*) AS count FROM `transaction` WHERE `status` = 'Check Out'",
+          function (err, result1) {
+            if (err) {
+              res.send({
+                status: 500,
+                message: "Something is wrong on our side",
+                error: err,
+              });
+            } else {
+              connection.query(
+                "SELECT COUNT(*) AS count FROM `transaction` WHERE `status` = 'Pending'",
+                function (err, result2) {
+                  if (err) {
+                    res.send({
+                      status: 500,
+                      message: "Something is wrong on our side",
+                      error: err,
+                    });
+                  } else {
+                    res.send({
+                      status: 200,
+                      message: "Successfully Fetched",
+                      check_in: result[0].count,
+                      check_out: result1[0].count,
+                      pending: result2[0].count,
+                    });
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+app.get("/checkin_reservation", (req, res) => {
+  connection.query(
+    "SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE `status` = 'Check In'",
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        res.send({
+          status: 200,
+          message: "Successfully Fetched",
+          data: result,
+        });
+      }
+    }
+  );
+});
+
+app.put("/confirm_checkout", (req, res) => {
+  const { transaction_id } = req.body;
+  console.log(transaction_id);
+  connection.query(
+    "UPDATE `transaction` SET `checkout_time` = ?, `status` = 'Check Out' WHERE `transaction_id` = ?",
+    [moment().format("HH:mm:ss"), transaction_id],
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      }
+      res.send({
+        status: 200,
+        message: "Successfully Checked Out",
+        data: result,
+      });
+    }
+  );
+});
+
+app.get("/checkout_reservation", (req, res) => {
+  connection.query(
+    "SELECT * FROM `transaction` NATURAL JOIN `guest` NATURAL JOIN `room` WHERE `status` = 'Check Out'",
+    function (err, result) {
+      if (err) {
+        res.send({
+          status: 500,
+          message: "Something is wrong on our side",
+          error: err,
+        });
+      } else {
+        res.send({
+          status: 200,
+          message: "Successfully Fetched",
+          data: result,
+        });
+      }
     }
   );
 });
